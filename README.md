16b94f0b7123a751e46c57cc6c3d5e695b7b4e54
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-08-04 18:11:04 -0500

    finally done with what i was trying to do. local storage works, horizontal rule works again. calling it good.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Giphy-Project
+
+## What is this?
+This is a GIF generator loosely based on movies, but you can create anything really. It's mobile responsive and should work on all screens sizes.
+
+## Why is this useful?
+This is useful for me to track my progress in my coding boot camp and remember some of the stuff I've done in the past. It may also be helpful to other new coders to see possible ways to do things.
+
+## How to get started?
+Simply click on a button at the top of the page to get a selection of GIFs. You are also able to create new buttons to get GIFs of your choosing. If you don't select a number it will default to 10, otherwise you can choose a number between 1 and 25. You are able to select favorites that move to another section of the screen and will be saved in localStorage until removed. They persist in case you want to come back to one later.
+
+## Where to get help?
+You can reach out to me diretly, but I make no claims as to how quickly I'll get back to you. The best bet is to just Google what you're stuck on. That's what I did to create this game. W3Schools, Mozilla's Develper Network, and Stack Overflow were really helpful resources.
+
+## Maintence and contributions
+This game may be periodically maintained by me and as of this writing, I'm the only contributor to it.
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -18,14 +18,13 @@ $(document).ready(function () {
             "Step Brothers",
             "Stargate"
         ],
-        persistMovies: [],
+        persistMovies: JSON.parse(localStorage.getItem("favorites")),
 
         // This doesn't work, I'd be curious to know if you could generate the url outside the AJAX call to and pass it into it, but I keep getting CORS errors.
         //    queryURL: function() {
         //        var url = "https://api.giphy.com/v1/gifs/search?api_key=3ImgVPYGAgw75lJTDGZ8FNGYVAnFqwCY&limit=10&q=Wedding+Crashers";
         //        return url
         //},
-        counter: 0,
         createButton: function (str) {
             var newButtonElem = $("<button>");
             $(newButtonElem).attr({
@@ -54,13 +53,15 @@ $(document).ready(function () {
             } else {
                 queryURL = queryURL + "&limit=10"
             }
+
             $.ajax({
                 url: queryURL,
                 method: "GET",
             }).then(function (response) {
                 var results = response.data;
+                console.log($("#movieGifsHere").children().length)
                 if ($("#movieGifsHere").children().length > 0) {
-                    $("#movieGifsHere").prepend("<hr>");
+                    $("#movieGifsHere").prepend("<hr class=\"w-100\">");
                 }
                 for (index in results) {
                     var newDivElem = $("<div>");
@@ -83,12 +84,11 @@ $(document).ready(function () {
                         "data-state": "still",
                         "data-still": results[index].images.fixed_height_still.url,
                         "data-animate": results[index].images.fixed_height.url,
+                        "data-rating": results[index].rating,
                         "class": "rounded gif"
                     });
-                    $(newDivElem).css("display", "inline-block");
                     $(newDivElem).attr({
-                        "class": "my-3 mx-auto mx-xl-3",
-                        "id": gifs.counter
+                        "class": "my-3 mx-auto mx-xl-3 gifContainer",
                     });
                     $(newSubDivElem).append(newPElem);
                     $(newSubDivElem).append(favoritesButtonElem);
@@ -114,32 +114,84 @@ $(document).ready(function () {
             }
         }),
         addToFavorites: $("#movieGifsHere").on("click", ".addFavorite", function () {
-            $(this).html("<img src=\"assets/images/broken-heart.png\" width=\"24px\">");
-            $(this).removeClass("btn-warning").addClass("btn-danger");
+            var store = {};
             var move = $(this).parents().eq(1)
-            if ($("#favorites").children().length === 0) {
-                $(move).removeClass("my-3").removeClass("mx-xl-3");
-            } else {
-                $(move).removeClass("mx-xl-3").removeClass("my-3").addClass("mb-xl-3");
-            }
-            $("#favorites").prepend(move);
+            store["dataAnimate"] = $(move).children("img").attr("data-animate");
+            store["dataStill"] = $(move).children("img").attr("data-still");
+            store["rating"] = $(move).children("img").attr("data-rating");
+            gifs.persistMovies.push(store);
+            localStorage.setItem("favorites", JSON.stringify(gifs.persistMovies));
+            $(move).remove();
+            gifs.renderFavorites();
         }),
+        renderFavorites: function () {
+            var clearButton = $("#clearFavorites");
+            $("#favorites").empty();
+            if (!Array.isArray(gifs.persistMovies)) {
+                gifs.persistMovies = [];
+            }
+            for (i in gifs.persistMovies) {
+                var outerDivElem = $("<div style=\"display: inline-block\" class=\"gifContainer\">");
+                var innerDivElem = $("<div>");
+                var pElem = $("<p class=\"rating mb-0\" style=\"display: inline; float: left\">");
+                var buttonElem = $("<button class=\"addFavorite btn btn-sm mb-1 btn-danger\" style=\"float: right\">");
+                var buttonImgElem = $("<img src=\"assets/images/broken-heart.png\" width=\"24px\">")
+                var imgElem = $("<img class=\"rounded gif\">");
+                var spanElem = $("<span class=\"font-weight-bold\">")
+                if ($("#favorites").children().length === 0) {
+                    $(outerDivElem).addClass("mx-auto");
+                } else {
+                    $(outerDivElem).addClass("mx-auto mb-xl-3");
+                }
+                $(spanElem).text("Rating: ");
+                $(outerDivElem).attr("data-storeID", i);
+                $(imgElem).attr({
+                    "data-still": gifs.persistMovies[i].dataStill,
+                    "data-animate": gifs.persistMovies[i].dataAnimate,
+                    "data-state": "still",
+                    "src": gifs.persistMovies[i].dataStill
+                })
+                $(pElem).append(spanElem);
+                $(pElem).append(gifs.persistMovies[i].rating)
+                $(buttonElem).append(buttonImgElem);
+                $(innerDivElem).append(pElem);
+                $(innerDivElem).append(buttonElem);
+                $(outerDivElem).append(innerDivElem);
+                $(outerDivElem).append(imgElem);
+                $("#favorites").prepend(outerDivElem);
+            }
+            if ($("#favorites").children().length === 0) {
+                $(clearButton).css("display", "none");
+            } else {
+                $(clearButton).css("display", "inline-block");
+            }
+        },
         removeFromFavorites: $("#favorites").on("click", ".addFavorite", function () {
+
             $(this).html("<img src=\"assets/images/star.png\" width=\"24px\">");
             $(this).removeClass("btn-danger").addClass("btn-warning");
             var move = $(this).parents().eq(1);
+            var removeID = $(move).attr("storeID");
             $(move).removeClass("mb-xl-3").addClass("mx-xl-3 my-3");
-            if ($("#" + ($(this).parents().eq(1).attr("id") - 1)).parent().attr("id") === "favorites") {
+            $("#movieGifsHere").prepend(move);
+            gifs.persistMovies.splice(removeID, 1);
+            localStorage.setItem("favorites", JSON.stringify(gifs.persistMovies));
+            gifs.renderFavorites();
+        }),
+        clearFavorites: $("#clearFavorites").on("click", function () {
+            localStorage.clear();
+            gifs.persistMovies = [];
+            $("#favorites .gifContainer").each(function (index, value) {
+                var move = $(this);
+                $(move).children().children("button").removeClass("btn-danger").addClass("btn-warning");
+                $(move).children().children().children("img").attr("src", "assets/images/star.png");
+                $(move).removeClass("mb-xl-3").addClass("mx-xl-3 my-3");
+                $(move).detach();
                 $("#movieGifsHere").prepend(move);
-            } else {
-                $(move).insertBefore($("#" + ($(this).parents().eq(1).attr("id") - 1)));
-            }
-            if ($("#favorites div:last-child").hasClass("mb-xl-3")) {
-                $("#favorites div:last-child").removeClass("mb-xl-3")
-            }
+                gifs.renderFavorites();
+            })
         })
     }
-
     $("#submit").on("click", function () {
         gifs.movies.push($("#movieInput").val().trim());
         $("#movieInput").val("");
@@ -147,4 +199,5 @@ $(document).ready(function () {
     });
 
     gifs.buttonArray(gifs.movies);
+    gifs.renderFavorites();
 })
